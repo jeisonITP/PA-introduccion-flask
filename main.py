28cf8d857290e93858218e1630002c3a4ac84e3d
Jeyson Calvache <jeison130@gmail.com>
2022-03-03 20:28:44 -0500

    Listado y formulario de productos

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 from flask import Flask, render_template
+import mysql.connector
+
+db = mysql.connector.connect(
+    host='172.17.3.7',
+    user='productos',
+    password='productos',
+    database='productos',
+    port=3306
+)
+db.autocommit = True
 
 app = Flask(__name__)
 
 @app.get("/")
 def index():
-    return render_template("index.html")
+    cursor = db.cursor(dictionary=True)
+    
+    cursor.execute('select * from productos')
+    
+    productos = cursor.fetchall()
+    #producto = cursor.fetchone()
+    cursor.close()
+    
+    return render_template("index.html", productos=productos)
+
+@app.get("/crear")
+def crearProducto():
+    return render_template("crear.html")
 
 @app.get("/contacto")
 def contacto():
